5b8e5160b5561d4d02d99fa9260ef5f9ab1aad97
Simon Cropp <simon.cropp@gmail.com>
2019-08-02 14:03:14 +1000

    add mediaservice

---
+++ src/Tests/MediaFeed/MediaFeedService.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+
+static class MediaFeedService
+{
+    static MediaFeedService()
+    {
+        using (var reader = File.OpenText(@"MediaFeed\aec-mediafeed-results-standard-verbose-24310.xml"))
+        {
+            var serializer = new XmlSerializer(typeof(MediaFeed));
+            Feed = (MediaFeed)serializer.Deserialize(reader);
+            HouseOfReps = GetHouseOfReps(Feed);
+        }
+    }
+
+    public static MediaFeedResultsElectionHouse HouseOfReps;
+
+    public static MediaFeed Feed;
+
+    static MediaFeedResultsElectionHouse GetHouseOfReps(MediaFeed feed)
+    {
+        return feed.Results.Election
+            .Where(x => x.ElectionIdentifier.Id == "H")
+            .Single()
+            .House;
+    }
+
+}
---
+++ src/Tests/MediaFeed/MediaFeedTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using Xunit;
+using Xunit.Abstractions;
+
+public class MediaFeedTests :
+    XunitLoggingBase
+{
+    [Fact]
+    public void Run()
+    {
+        foreach (var contest in MediaFeedService.HouseOfReps.Contests)
+        {
+            var electorate = contest.ContestIdentifier.ContestName;
+            Debug.WriteLine(electorate);
+            var candidatePreferred = contest.TwoCandidatePreferred;
+            foreach (var candidate in candidatePreferred.Candidate)
+            {
+                if (candidate.AffiliationIdentifier == null)
+                {
+                    Debug.WriteLine($"    {candidate.CandidateIdentifier.CandidateName} {candidate.Votes.Swing}");
+                }
+                else
+                {
+                    Debug.WriteLine($"    {candidate.AffiliationIdentifier.ShortCode} {candidate.Votes.Swing}");
+                }
+            }
+        }
+    }
+
+    public MediaFeedTests(ITestOutputHelper output) :
+        base(output)
+    {
+    }
+}
