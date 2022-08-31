4c5dfc5ad141c64dff6ce14598a29268b9004dc1
Simon <simon.cropp@gmail.com>
2022-08-31 13:06:52 +1000

    .

---
+++ src/Tests/ConvertToSqlGeometryTests.cs
@@ -0,0 +1,45 @@
+﻿using NetTopologySuite.Algorithm.Locate;
+using NetTopologySuite.Features;
+using NetTopologySuite.Geometries;
+using NetTopologySuite.IO;
+using Newtonsoft.Json;
+using Xunit.Abstractions;
+
+public class ConvertToSqlGeometryTests
+{
+    private readonly ITestOutputHelper output;
+
+    public ConvertToSqlGeometryTests(ITestOutputHelper output)
+    {
+        this.output = output;
+    }
+    [Fact]
+    public void Foo()
+    {
+        Dictionary<string, IPointOnGeometryLocator> locators = new Dictionary<string, IPointOnGeometryLocator>();
+        var electorateDir = Path.Combine(DataLocations.Maps2022Path,"Electorates");
+        foreach (var path in Directory.EnumerateFiles(electorateDir,"*.geojson").Where(_=>!_.Contains('_')))
+        {
+            var serializer = GeoJsonSerializer.Create(new GeometryFactoryEx());
+            using var jsonReader = new JsonTextReader(File.OpenText(path));
+            var featureCollection = serializer.Deserialize<FeatureCollection>(jsonReader);
+            var feature = featureCollection.Single();
+            var geometry = feature.Geometry;
+            locators.Add(path, new IndexedPointInAreaLocator(geometry));
+        }
+
+        var startNew = Stopwatch.StartNew();
+        foreach (var locator in locators)
+        {
+            var location = locator.Value.Locate(new Coordinate(14.09, -35.349));
+
+            var isInside = !location.HasFlag(Location.Exterior);
+            if (isInside)
+            {
+                output.WriteLine(locator.Key);
+            }
+        }
+        output.WriteLine(startNew.ElapsedMilliseconds.ToString());
+    }
+
+}
